08bbe176ecbaba32ceeecd1af6eafee7b7e1838a
Hyeonsoo0625 <hyeonsoo02@gmail.com>
2023-12-22 23:50:38 +0900

    Feat: swagger

---
+++ src/main/java/com/example/UMC_spring/web/dto/MemberRequestDTO.java
@@ -0,0 +1,20 @@
+package com.example.UMC_spring.web.dto;
+
+import lombok.Getter;
+
+import java.util.List;
+
+public class MemberRequestDTO {
+
+    @Getter
+    public static class JoinDto{
+        String name;
+        Integer gender;
+        Integer birthYear;
+        Integer birthMonth;
+        Integer birthDay;
+        String address;
+        String specAddress;
+        List<Long> preferCategory;
+    }
+}
